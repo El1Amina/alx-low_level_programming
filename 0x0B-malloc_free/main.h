bc3dbdf8de1ce1fcbc5c05fa9d794f374b4b4a45
Amina El <aminaelgharbaoui2gmail.com>
2023-05-15 15:42:36 -0700

    0x0B. C - malloc, free

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,5 +8,6 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char *create_array(unsigned int size, char c);
+char **strtow(char *str);
 
 #endif
